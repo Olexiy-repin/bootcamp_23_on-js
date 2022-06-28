7118e15ad878728dedb7657379889df2c38c08ab
Olexiy-repin <31596886+Olexiy-repin@users.noreply.github.com>
2022-06-28 14:46:37 +0300

    classwork

--- js/callbacks.js
+++ js/callbacks.js
@@ -130,47 +130,47 @@
 ? інакше додаємо до балансу amount і викликаємо onSuccess(`Account balance: ${this.balance}`)
 */
 
-const TRANSACTION_LIMIT = 1000;
+// const TRANSACTION_LIMIT = 1000;
 
-const account = {
-  username: 'Jacob',
-  balance: 400,
+// const account = {
+//   username: 'Jacob',
+//   balance: 400,
 
-  withdraw(amount, onSuccess, onError) {
-    if (amount > TRANSACTION_LIMIT) {
-      onError(`Amount should not exceed ${TRANSACTION_LIMIT} credits`);
-    } else if (amount > this.balance) {
-      onError(`Amount can't exceed account balance of ${this.balance} credits`);
-    } else {
-      this.balance -= amount;
-      onSuccess(`Account balance: ${this.balance}`);
-    }
-  },
+//   withdraw(amount, onSuccess, onError) {
+//     if (amount > TRANSACTION_LIMIT) {
+//       onError(`Amount should not exceed ${TRANSACTION_LIMIT} credits`);
+//     } else if (amount > this.balance) {
+//       onError(`Amount can't exceed account balance of ${this.balance} credits`);
+//     } else {
+//       this.balance -= amount;
+//       onSuccess(`Account balance: ${this.balance}`);
+//     }
+//   },
 
-  deposit(amount, onSuccess, onError) {
-    if (amount > TRANSACTION_LIMIT) {
-      onError(`Amount should not exceed ${TRANSACTION_LIMIT} credits`);
-    } else if (amount <= 0) {
-      onError(`Amount must be more than 0 credits`);
-    } else {
-      this.balance += amount;
-      onSuccess(`Account balance: ${this.balance}`);
-    }
-  },
-};
+//   deposit(amount, onSuccess, onError) {
+//     if (amount > TRANSACTION_LIMIT) {
+//       onError(`Amount should not exceed ${TRANSACTION_LIMIT} credits`);
+//     } else if (amount <= 0) {
+//       onError(`Amount must be more than 0 credits`);
+//     } else {
+//       this.balance += amount;
+//       onSuccess(`Account balance: ${this.balance}`);
+//     }
+//   },
+// };
 
-function handleSuccess(message) {
-  console.log(`✅ Success! ${message}`);
-}
+// function handleSuccess(message) {
+//   console.log(`✅ Success! ${message}`);
+// }
 
-function handleError(message) {
-  console.log(`❌ Error! ${message}`);
-}
+// function handleError(message) {
+//   console.log(`❌ Error! ${message}`);
+// }
 
-account.withdraw(2000, handleSuccess, handleError);
-account.withdraw(600, handleSuccess, handleError);
-account.withdraw(300, handleSuccess, handleError);
-account.deposit(1700, handleSuccess, handleError);
-account.deposit(0, handleSuccess, handleError);
-account.deposit(-600, handleSuccess, handleError);
-account.deposit(600, handleSuccess, handleError);
+// account.withdraw(2000, handleSuccess, handleError);
+// account.withdraw(600, handleSuccess, handleError);
+// account.withdraw(300, handleSuccess, handleError);
+// account.deposit(1700, handleSuccess, handleError);
+// account.deposit(0, handleSuccess, handleError);
+// account.deposit(-600, handleSuccess, handleError);
+// account.deposit(600, handleSuccess, handleError);
