b6ddd02e93f140068c22f7a33b37ab1186dfcb7d
Olexiy-repin <31596886+Olexiy-repin@users.noreply.github.com>
2022-07-10 22:44:36 +0300

    classwork

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+class User {
+  constructor(username, userEmail) {
+    this.username = username;
+    this.userEmail = userEmail;
+  }
+}
+
+const user = new User('Mae Todd', 'nucuwis@omte.ae');
